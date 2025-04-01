0904083701ecb946db9f5f357622a08f2d7cb182
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-01 19:30:31 -0300

    Eu amo somar

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double number1;
+double number2;
+double soma;
+
+Console.Clear();
+
+Console.WriteLine("--- Cálculo da soma entre dois números ---");
+
+Console.Write(@"
+Digite o primeiro número: ");
+number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o primeiro número: ");
+number2 = Convert.ToDouble(Console.ReadLine());
+
+soma = number1 + number2;
+
+Console.WriteLine(@$"
+Soma: {soma}");
